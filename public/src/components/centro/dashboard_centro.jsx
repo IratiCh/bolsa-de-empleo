b78b5b7a3eac5c7d5b352d1673e5436cdae4e4da
IratiCh <ichaverast1@educacion.navarra.es>
2026-03-26 17:52:25 +0100

    Change footer class from 'index-footer' to 'global-footer'

--- public/src/components/centro/dashboard_centro.jsx
+++ public/src/components/centro/dashboard_centro.jsx
@@ -160,7 +160,7 @@ const DashboardCentro = () => {
                 </div>
             </div>
 
-            <footer className="index-footer">
+            <footer className="global-footer">
                 <div className="footer-container">
                     <p>© 2026 Sara & Irati. Derechos reservados.</p>
                 </div>
