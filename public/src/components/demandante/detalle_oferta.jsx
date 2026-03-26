9bf153b56d415f51ccd139a2eb54ff3dd9b2fbc8
IratiCh <ichaverast1@educacion.navarra.es>
2026-03-26 17:51:24 +0100

    Change footer class from 'index-footer' to 'global-footer'

--- public/src/components/demandante/detalle_oferta.jsx
+++ public/src/components/demandante/detalle_oferta.jsx
@@ -256,7 +256,7 @@ function DetalleOferta() {
                 )}
             </div>
 
-            <footer className="index-footer">
+            <footer className="global-footer">
                 <div className="footer-container">
                     <p>© 2026 Sara & Irati. Derechos reservados.</p>
                 </div>
