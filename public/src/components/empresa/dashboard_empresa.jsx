114cc7420df9f773ef486b51c61be761f7c9b62d
IratiCh <ichaverast1@educacion.navarra.es>
2026-03-26 17:50:36 +0100

    Change footer class from 'index-footer' to 'global-footer'

--- public/src/components/empresa/dashboard_empresa.jsx
+++ public/src/components/empresa/dashboard_empresa.jsx
@@ -355,7 +355,7 @@ function DashboardEmpresa() {
                 </div>
             </div>
 
-            <footer className="index-footer">
+            <footer className="global-footer">
                 <div className="footer-container">
                     <p>© 2026 Sara & Irati. Derechos reservados.</p>
                 </div>
