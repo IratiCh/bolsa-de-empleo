cc024e29eb620f5a3828d216e4f0bee789355058
IratiCh <ichaverast1@educacion.navarra.es>
2026-03-26 17:52:04 +0100

    Change footer class from 'index-footer' to 'global-footer'

--- public/src/components/centro/activar_empresas.jsx
+++ public/src/components/centro/activar_empresas.jsx
@@ -166,7 +166,7 @@ const ActivarEmpresas = () => {
                 </div>
             </div>
 
-            <footer className="index-footer">
+            <footer className="global-footer">
                 <div className="footer-container">
                     <p>© 2026 Sara & Irati. Derechos reservados.</p>
                 </div>
