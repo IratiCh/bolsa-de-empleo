dea76750d68d335e8ddbe25bfd556cac1de2bc30
IratiCh <ichaverast1@educacion.navarra.es>
2026-03-26 17:52:15 +0100

    Change footer class from 'index-footer' to 'global-footer'

--- public/src/components/centro/crear_titulo.jsx
+++ public/src/components/centro/crear_titulo.jsx
@@ -109,7 +109,7 @@ const CrearTitulo = () => {
                 </div>
             </div>
 
-            <footer className="index-footer">
+            <footer className="global-footer">
                 <div className="footer-container">
                     <p>© 2026 Sara & Irati. Derechos reservados.</p>
                 </div>
